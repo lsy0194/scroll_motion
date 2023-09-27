ed6d213925898d802bd95190c928d2f2efb8a629
lsy0194 <tktmadltmddu@naver.com>
2023-09-27 11:26:52 +0900

    scroll값 보정해주는 함수재활용

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 /*
-  window.scoolY : 브라우저 스크롤시 스크롤되는 거리값(동적)
-  DOM.offset().top : 전체페이지에 특정 DOM의 세로 위치값(정적)
+  window.scrollY : 브라우저 스크롤시 스크롤되는 거리값 (동적)
+  DOM.offsetTop : 전체 페이지에 특정 DOM의 세로 위치값 (정적)
 */
 const secs = document.querySelectorAll('section');
 const h1 = secs[0].querySelector('h1');
 const h1_2 = secs[1].querySelector('h1');
-const baseline = 300;
-//baseline은 박스의 모션 시작기점을 기존 박스 영역에서 300만큼위로올림
+const baseLine = -window.innerHeight / 2;
+
 window.addEventListener('scroll', () => {
-	const scroll = window.scrollY;
-
-	//박스에 실적용되는 스크롤 수치값을 반대로 300만큼 더해줘야됨
-
-	h1.style.transform = `translateX(${scroll}px) rotate(${scroll}deg) scale(${1 + scroll / 400})`;
-	h1.style.opacity = 1 - scroll / 400;
-
-	let scroll_2 = 0;
-	//스크롤 거리값이 2번째 섹션영역에 들어가면 scroll적용되는 값을 담고
-	//반대로 2번째 섹션영역을 벗어나면 scroll_2의 값을 0으로 초기화
-	scroll >= secs[1].offsetTop - baseline
-		? (scroll_2 = scroll - secs[1].offsetTop + baseline)
-		: (scroll_2 = 0);
-	h1_2.style.transform = `translateX(${scroll_2}px) rotate(${scroll_2}deg) scale(${
-		1 + scroll_2 / 400
-	})`;
-	h1_2.style.opacity = 1 - scroll_2 / 400;
+	//const scrolled1 = setScroll(secs[0]);
+	h1.style.transform = `translateX(${setScroll(secs[0])}px)`;
+	h1_2.style.transform = `translateX(${setScroll(secs[1], -window.innerHeight / 2)}px)`;
 });
+
+//첫번째인수로 기준점이 될 frame요소 지정, 두번째 인수로 스크롤 모션이 적용될 커스텀 시점지정 (기본값 0)
+
+function setScroll(frame, baseLine = 0) {
+	//내부적으로 scroll값 동적생성
+	const scroll = window.scrollY;
+	let scroll2 = 0;
+	//첫번째와 두번째 인수로 넘어오는 부모세로위치값과 base보정값을 적용한 영역에 도달하면 scroll값 변경시작
+	//만약 벗어나면 원래 위치값으로 scroll값 초기화
+
+	scroll >= frame.offsetTop + baseLine
+		? (scroll2 = scroll - frame.offsetTop - baseLine)
+		: (scroll2 = 0);
+	//위에서 만들어지고 있는 스크로값을 실시간으로 리턴
+
+	return scroll2;
+}
