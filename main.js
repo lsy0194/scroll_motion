f81f798af1ee759b30f259d074fcd15c9911051c
lsy0194 <tktmadltmddu@naver.com>
2023-09-27 10:40:04 +0900

    특정 영역에 도달했을때 스크롤 적용되는 값만 3항연산자로 간소하게 처리

--- main.js
+++ main.js
@@ -9,22 +9,20 @@ const baseline = 300;
 //baseline은 박스의 모션 시작기점을 기존 박스 영역에서 300만큼위로올림
 window.addEventListener('scroll', () => {
 	const scroll = window.scrollY;
+
 	//박스에 실적용되는 스크롤 수치값을 반대로 300만큼 더해줘야됨
-	const scroll_2 = scroll - secs[1].offsetTop + baseline;
-	const initScroll = 0;
+
 	h1.style.transform = `translateX(${scroll}px) rotate(${scroll}deg) scale(${1 + scroll / 400})`;
 	h1.style.opacity = 1 - scroll / 400;
 
-	if (scroll >= secs[1].offsetTop - baseline) {
-		h1_2.style.transform = `translateX(${scroll_2}px) rotate(${scroll_2}deg) scale(${
-			1 + scroll_2 / 400
-		})`;
-		h1_2.style.opacity = 1 - scroll_2 / 400;
-	} else {
-		//원래위치로 스크롤시 조금씩 오차범위가 발생하기 떄문에 원래 값으로 강제 보정처리
-		h1_2.style.transform = `translateX(${initScroll}px) rotate(${initScroll}deg) scale(${
-			1 + initScroll / 400
-		})`;
-		h1_2.style.opacity = 1 - initScroll / 400;
-	}
+	let scroll_2 = 0;
+	//스크롤 거리값이 2번째 섹션영역에 들어가면 scroll적용되는 값을 담고
+	//반대로 2번째 섹션영역을 벗어나면 scroll_2의 값을 0으로 초기화
+	scroll >= secs[1].offsetTop - baseline
+		? (scroll_2 = scroll - secs[1].offsetTop + baseline)
+		: (scroll_2 = 0);
+	h1_2.style.transform = `translateX(${scroll_2}px) rotate(${scroll_2}deg) scale(${
+		1 + scroll_2 / 400
+	})`;
+	h1_2.style.opacity = 1 - scroll_2 / 400;
 });
