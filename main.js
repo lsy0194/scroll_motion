d34fc5f6ee6e9b695947c701ec2aa22838f142d3
lsy0194 <tktmadltmddu@naver.com>
2023-09-27 13:40:48 +0900

    callback모션 추r가

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const box = document.querySelector('article');
 box.addEventListener('click', () => {
 	new Anime(
 		box, //선택자
-		{ left: 300, backgroundColor: '#ff69b4', width: 400, opacity: 0 }, //모션적용할 속성값
-		{ duration: 1000, easeType: 'ease1' } //duration : 시간,easeType : 가속도,callback : 첫번째 모션이 끝나고 이어서 실행할 콜백함수
+		{ left: 300, backgroundColor: '#ff69b4' },
+		{
+			duration: 1000,
+			easeType: 'linear',
+			callback: () => {
+				new Anime(box, { top: 300, backgroundColor: '#000000' });
+			},
+		}
 	);
 });
